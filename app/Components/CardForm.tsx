7441b5a2ba2fe995f00fa4a4a0261d35c0350eff
Abdul Raheem <raheemveer5@gmail.com>
2024-12-20 20:40:16 +0500

    made a responsive payment method page

--- app/Components/CardForm.tsx
+++ app/Components/CardForm.tsx
@@ -3,7 +3,7 @@ import BillingInfoLabel from "../Components/BillingInfoLabel";
 
 export default function CardForm() {
   return (
-    <form className="grid grid-cols-2 gap-5 mt-5">
+    <form className="grid grid-cols-1 sm:grid-cols-2 gap-5 mt-5">
      <BillingInfoLabel content="Card Number" inputType="number" holder="Card Number" />
      <BillingInfoLabel content="Expiration Date" inputType="number" holder="DD/MM/YY" />
      <BillingInfoLabel content="Card Holder" inputType="number" holder="Card Holder" />
--- app/PaymentMethod/page.tsx
+++ app/PaymentMethod/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import BillingInfoLabel from "../Components/BillingInfoLabel";
 import RentalForm from "../Components/RentalForm";
 import CardOption from "../Components/CardOption";
-import TypeCapacityLabel from "../Components/TypeCapacityLabel";
 import Button from "../Components/Button";
 import shell from "../../public/images/shell.png";
 import Image from "next/image";
@@ -11,15 +10,15 @@ import ReviewStar from "../Components/ReviewStar";
 
 export default function page() {
   return (
-    <section className="flex gap-5 bg-[#F6F7F9] py-5 px-4 lg:px-10 min-h-[100vh]">
-      <div className="flex flex-col gap-5 w-[60%]">
-        <div className="bg-white px-5 py-6 rounded-md">
+    <section className="flex flex-col-reverse lg:flex-row gap-5 bg-[#F6F7F9] py-5 px-2 lg:px-10 min-h-[100vh]">
+      <div className="flex flex-col gap-5 w-full lg:w-[60%]">
+        <div className="bg-white px-3 sm:px-5 py-6 rounded-md">
           <div className="flex justify-between items-center">
             <div><h1 className="text-lg font-semibold">Billing Info</h1><p className="text-sm text-[#90A3BF]">Please Enter Your Billing info</p></div>
             <p className="text-sm text-[#90A3BF]">Step 1of4</p>
           </div>
 
-          <form className="grid grid-cols-2 gap-5 mt-8">
+          <form className="grid grid-cols-1 sm:grid-cols-2 gap-5 mt-8">
             <BillingInfoLabel content="Name" inputType="text" holder="Your Name" bgColor="bg-[#F6F7F9]"/>
             <BillingInfoLabel content="Phone Number" inputType="number" holder="Phone number" bgColor="bg-[#F6F7F9]"/>
             <BillingInfoLabel content="Address" inputType="text" holder="Address" bgColor="bg-[#F6F7F9]"/>
@@ -27,7 +26,7 @@ export default function page() {
           </form>
         </div>
 
-        <div className="bg-white px-5 py-6 rounded-md">
+        <div className="bg-white px-3 sm:px-5 py-6 rounded-md">
           <div className="flex justify-between items-center">
             <div><h1 className="text-lg font-semibold">Rental Info</h1><p className="text-sm text-[#90A3BF]">Please Enter Your Rental Date</p></div>
             <p className="text-sm text-[#90A3BF]">Step 2of4</p>
@@ -36,7 +35,7 @@ export default function page() {
           <RentalForm type="Drop-Off"/>
         </div>
 
-        <div className="bg-white px-5 py-6 rounded-md">
+        <div className="bg-white px-3 sm:px-5 py-6 rounded-md">
           <div className="flex justify-between items-center mb-8">
             <div><h1 className="text-lg font-semibold">Payment Method</h1><p className="text-sm text-[#90A3BF]">Please Enter Your payment method</p></div>
             <p className="text-sm text-[#90A3BF]">Step 3of4</p>
@@ -44,29 +43,29 @@ export default function page() {
           <CardOption />
         </div>
 
-        <div className="bg-white px-5 py-6 rounded-md">
+        <div className="bg-white px-3 sm:px-5 py-6 rounded-md">
           <div className="flex justify-between items-center mb-6">
-            <div><h1 className="text-lg font-semibold">Confirmation</h1><p className="text-sm text-[#90A3BF]">We are getting to the end. Just few clicks and your rental is ready!</p></div>
+            <div><h1 className="text-lg font-semibold">Confirmation</h1><p className="text-sm text-[#90A3BF] w-[230px] sm:w-full">We are getting to the end. Just few clicks and your rental is ready!</p></div>
             <p className="text-sm text-[#90A3BF]">Step 4of4</p>
           </div>
           <div className="flex flex-col gap-4 mb-6">
-            <div className="bg-[#F6F7F9] px-3 py-2 rounded-md"><TypeCapacityLabel carTypeOrSpace="I agree with sending an Marketing and newsletter emails. No spam, promissed!"/></div>
-            <div className="bg-[#F6F7F9] px-3 py-2 rounded-md"><TypeCapacityLabel carTypeOrSpace="I agree with our terms and conditions and privacy policy."/></div>
+            <div className="bg-[#F6F7F9] px-3 py-2 rounded-md flex gap-2 items-center"><input type="checkbox" className="w-4 h-4" /><span className="text-sm">I agree with sending an Marketing and newsletter emails. No spam, promissed!</span></div>
+            <div className="bg-[#F6F7F9] px-3 py-2 rounded-md flex gap-2 items-center"><input type="checkbox" className="w-4 h-4" /><span className="text-sm">I agree with our terms and conditions and privacy policy.</span></div>
           </div>
           <Button stylee="bg-[#3563E9] px-3 py-2 rounded-md" contentStyle="text-white text-sm" content="Rent Now"/>
           <div className="mt-6">
            <Image src={shell} alt='arrow down' className='w-6'/>
-           <h1 className="text-base font-semibold mt-4">All your data are safe</h1>
+           <h1 className="text-base font-semibold mt-4">All gap-2 your data are safe</h1>
            <p className="text-[#90A3BF]">We are using the most advanced security to provide you the best experience ever.</p>
           </div>
         </div>
       </div>
 
-      <div className="w-[40%] bg-white py-6 px-5 h-max rounded-md">
+      <div className="w-full lg:w-[40%] bg-white py-6 px-3 sm:px-5 h-max rounded-md">
         <h1 className="text-[#1A202C] text-xl font-semibold">Rental Summary</h1>
         <p className="text-[#90A3BF] text-base mt-3">Prices may change depending on the length of the rental and the price of your rental car.</p>
-        <div className="flex gap-5 mt-7">
-          <div className="bg-[url(/images/bg2.png)] bg-center bg-cover rounded-md flex justify-center items-center p-4"><Image src={gtr} alt="car interior" className="w-[150px]"/></div>
+        <div className="flex flex-col-reverse sm:flex-row gap-5 mt-7">
+          <div className="bg-[url(/images/bg2.png)] bg-center w-[200px] bg-cover rounded-md flex justify-center items-center p-4"><Image src={gtr} alt="car interior" className="w-[150px]"/></div>
           <div className="flex flex-col">
             <h1 className="text-[24px] font-semibold">Nissan GT-R</h1>
             <div className="flex items-center gap-2"><ReviewStar/><p className="text-sm text-[#6B7280]">440+ Reviewers</p></div>
@@ -81,8 +80,8 @@ export default function page() {
         </div>
 
        <div className="flex mt-5">
-         <input type="text" className="bg-[#F6F7F9] w-full outline-none text-[#90A3BF] py-2 px-3" placeholder="Apply promo code"/>
-         <Button stylee="bg-[#F6F7F9] py-2 px-2 w-[120px]" contentStyle="text-black text-sm" content="Apply Now"/>
+         <input type="text" className="bg-[#F6F7F9] w-full outline-none text-[#90A3BF] px-2 py-2" placeholder="Apply promo code"/>
+         <Button stylee="bg-[#F6F7F9] py-2 px-2 w-[140px]" contentStyle="text-black text-sm" content="Apply Now"/>
        </div>
 
        <div className="flex justify-between items-center mt-10">
